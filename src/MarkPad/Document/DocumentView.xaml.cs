1106e13b1d4687e9bc6665c34da14b74b19e7325
Andrew Tobin <andrew@grrargh.com>
2012-01-05 20:33:50 +1100

    Ctrl+B and Ctrl+I working for Bold/Italics

--- src/MarkPad/Document/DocumentView.xaml.cs
+++ src/MarkPad/Document/DocumentView.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Reflection;
+﻿using System.Linq;
+using System.Reflection;
 using System.Windows;
+using System.Windows.Input;
 using System.Xml;
+using ICSharpCode.AvalonEdit.Editing;
 using ICSharpCode.AvalonEdit.Highlighting;
 using ICSharpCode.AvalonEdit.Highlighting.Xshd;
 
@@ -21,6 +24,75 @@ namespace MarkPad.Document
             {
                 Document.SyntaxHighlighting = HighlightingLoader.Load(reader, HighlightingManager.Instance);
             }
+
+            var editCommandBindings = Document.TextArea.DefaultInputHandler.Editing.CommandBindings;
+            foreach (var binding in editCommandBindings.Where(binding => binding.Command == ICSharpCode.AvalonEdit.AvalonEditCommands.IndentSelection))
+            {
+                editCommandBindings.Remove(binding);
+                break;
+            }
+
+            Document.TextArea.KeyDown += TextArea_KeyDown;
+        }
+
+        void TextArea_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.B && Keyboard.Modifiers == ModifierKeys.Control)
+            {
+                BoldSelectedArea();
+                e.Handled = true;
+            }
+
+            else if (Keyboard.Modifiers == ModifierKeys.Control)
+            {
+                if (e.Key == Key.I)
+                {
+                    ItalicsSelectedArea();
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private string RetrieveSelectedArea()
+        {
+            var textArea = this.Document.TextArea;
+            if (textArea.Selection.IsEmpty)
+            {
+                var line = textArea.Document.GetLineByOffset(textArea.Caret.Offset);
+                textArea.Selection = textArea.Selection.StartSelectionOrSetEndpoint(line.Offset, line.Offset + line.Length);
+            }
+
+            return textArea.Selection.GetText(textArea.Document);
+        }
+
+        public void BoldSelectedArea()
+        {
+            string selectedText = RetrieveSelectedArea();
+
+            if ((selectedText.StartsWith("**") && selectedText.EndsWith("**")) ||
+                (selectedText.StartsWith("***") && selectedText.EndsWith("***")))
+                Document.TextArea.Selection.ReplaceSelectionWithText(Document.TextArea, selectedText.Replace("**", ""));
+            else Document.TextArea.Selection.ReplaceSelectionWithText(Document.TextArea, "**" + selectedText + "**");
+
+        }
+
+        public void ItalicsSelectedArea()
+        {
+            var selectedText = RetrieveSelectedArea();
+
+            if (selectedText.StartsWith("***") &&
+                selectedText.EndsWith("***"))
+            {
+                Document.TextArea.Selection.ReplaceSelectionWithText(Document.TextArea, selectedText.Replace("***", "**"));
+            }
+            else if (selectedText.StartsWith("*") &&
+                selectedText.EndsWith("*") &&
+                !selectedText.StartsWith("**") &&
+                !selectedText.EndsWith("**"))
+            {
+                Document.TextArea.Selection.ReplaceSelectionWithText(Document.TextArea, selectedText.Replace("*", ""));
+            }
+            else Document.TextArea.Selection.ReplaceSelectionWithText(Document.TextArea, "*" + selectedText + "*");
         }
     }
 }
